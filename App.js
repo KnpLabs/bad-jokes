216bb3b3813ccb1e526820b1400ae63a3f2b6f92
RwanPrawn <erwan.vincent@knplabs.com>
2018-02-08 11:56:58 +0100

    Redux and Navigation bootstrap

--- App.js
+++ App.js
@@ -1,90 +1,25 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
-import { Icon } from 'react-native-elements';
-import { RkTheme, RkTabView, RkButton, RkCard, RkText } from 'react-native-ui-kitten'
+import { Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { Home } from './src/Home';
+import { NewGame } from './src/NewGame';
 
-/**
- * Customize theme
- */
-RkTheme.setType('RkCard', 'story', {
-    img: {
-        height: 100,
-        opacity: 0.7
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: Home
     },
-    header: {
-        alignSelf: 'center'
-    },
-    content:{
-        alignSelf:'center'
+    NewGame: {
+      screen: NewGame
     }
-});
+  },
+  {
+    initialRouteName: 'Home'
+  }
+)
 
-/**
- * Generic renderers
- */
-let renderTab = (isSelected, title, icon) => {
-    let backgroundColor = isSelected ? 'cornflowerblue' : 'white';
-    let color = (!isSelected) ? 'cornflowerblue' : 'white';
-
-    return (
-        <View
-            style={{
-                backgroundColor,
-                justifyContent: 'center',
-                flexDirection: 'row',
-                alignItems: 'center',
-                padding: 17,
-            }}>
-            <Icon name={icon} type="font-awesome" style={{color, fontSize: 16}}/>
-            <RkText style={{color, marginLeft: 11}}>{title}</RkText>
-        </View>);
-};
-
-/**
- * Tabs renderers
- */
-let renderHome = () => {
-    return (
-        <RkCard rkType='story'>
-            <Image rkCardImg source={require('./img/sea.jpg')}/>
-            <View rkCardHeader>
-                <RkText rkType='header'>Once upon a time</RkText>
-            </View>
-            <View rkCardContent>
-                <RkText style={{textAlign:'center'}}>
-                    One morning, when Gregor Samsa woke from happy dreams,
-                    he found himself transformed in ...
-                </RkText>
-            </View>
-            <View rkCardFooter>
-                <RkButton rkType='small outline'>Learn More</RkButton>
-                <RkButton rkType='small'>Read later</RkButton>
-            </View>
-        </RkCard>
-    );
-};
-
-/**
- * THE App !
- */
 const App = () => (
-    <RkTabView rkType='material' tabsUnderContent='true' >
-        <RkTabView.Tab title={(selected) => {
-            return renderTab(selected, 'Bus', 'youtube');
-        }}>
-            <Text>Bus</Text>
-        </RkTabView.Tab>
-        <RkTabView.Tab title={(selected) => {
-            return renderTab(selected, 'Train', 'facebook');
-        }}>
-            <Text>Train</Text>
-        </RkTabView.Tab>
-        <RkTabView.Tab title={(selected) => {
-            return renderTab(selected, 'Plain', 'twitter');
-        }}>
-            <Text>Plain</Text>
-        </RkTabView.Tab>
-    </RkTabView>
-);
+  <RootStack />
+)
 
 export default App;
---
+++ src/actions/jokes.js
@@ -0,0 +1,7 @@
+export const LOAD_JOKE = 'LOAD_JOKE';
+
+export function loadQuestion() {
+  return {
+    type: LOAD_JOKE,
+  }
+}
