ee167d6cc485f19cb08d25e16fa153db9885a8ba
Antoine Lelaisant <antoine.lelaisant@knplabs.com>
2018-02-10 10:14:58 +0100

    Fixed indentation

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -4,29 +4,29 @@ import { RkText } from 'react-native-ui-kitten';
 import { globalStyles } from '../styles/Global';
 
 export const Greeting = (props) => {
-    const teams = props.teams;
+  const teams = props.teams;
 
-    if (teams.first.score > teams.second.score) {
-        return (
-            <View>
-                <RkText style={globalStyles.finishStrong}>{teams.first.name}</RkText>
-                <RkText style={globalStyles.finishText}>a gagné !</RkText>
-            </View>
-        );
-    } else if (teams.first.score < teams.second.score) {
-        return (
-            <View>
-                <RkText style={globalStyles.finishStrong}>{teams.second.name}</RkText>
-                <RkText style={globalStyles.finishText}>a gagné !</RkText>
-            </View>
-        );
-    }
-
-    // Same score => equality
+  if (teams.first.score > teams.second.score) {
     return (
-        <View>
-            <RkText style={globalStyles.finishStrong}>Égalité&nbsp;!</RkText>
-            <RkText style={globalStyles.finishText}>Bande de losers&nbsp;!</RkText>
-        </View>
+      <View>
+        <RkText style={globalStyles.finishStrong}>{teams.first.name}</RkText>
+        <RkText style={globalStyles.finishText}>a gagné !</RkText>
+      </View>
     );
+  } else if (teams.first.score < teams.second.score) {
+    return (
+      <View>
+        <RkText style={globalStyles.finishStrong}>{teams.second.name}</RkText>
+        <RkText style={globalStyles.finishText}>a gagné !</RkText>
+      </View>
+    );
+  }
+
+  // Same score => equality
+  return (
+    <View>
+      <RkText style={globalStyles.finishStrong}>Égalité&nbsp;!</RkText>
+      <RkText style={globalStyles.finishText}>Bande de losers&nbsp;!</RkText>
+    </View>
+  );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,17 @@ import { globalStyles } from '../styles/Global';
 
 export const Home = ({navigation}) => (
   <View style={globalStyles.container}>
-    <Image style={globalStyles.homeImg}
-           source={require('../assets/img/logo.jpg')} />
-    <RkButton style={[globalStyles.bigBottomBtn, globalStyles.homeBtn]}
-              contentStyle={globalStyles.boldBtnContent}
-              rkType='xlarge'
-              onPress={() => navigation.navigate('Player')}><Text style={{ color: 'white', fontSize: 20 }}>Démarrer une partie</Text></RkButton>
+    <Image
+      style={globalStyles.homeImg}
+      source={require('../assets/img/logo.jpg')}
+    />
+    <RkButton
+      style={[globalStyles.bigBottomBtn, globalStyles.homeBtn]}
+      contentStyle={globalStyles.boldBtnContent}
+      rkType='xlarge'
+      onPress={() => navigation.navigate('Player')}
+    >
+      <Text style={{ color: 'white', fontSize: 20 }}>Démarrer une partie</Text>
+    </RkButton>
   </View>
 );
